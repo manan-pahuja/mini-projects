c453124312c1eb59b1783b22c61a290bac8aede6
manan-pahuja <pahuja.manan@outlook.com>
2026-08-24 12:14:30 +0530

    Update main.cpp

--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+// Header file for input/output operations
 
+// The main function is where the program execution starts
 int main() 
 {
     std::cout << "Hello World! " << std::endl;
+
+/* 
+       std::cout is used to print output to the screen.
+       '<<' is the insertion operator.
+       'std::endl' creates a new line.
+    */
     return 0;
+    // Signals that the program executed successfully
 
 }
