5ed6d7299155f0e0cbcb4ceba1279447f6a29343
manan-pahuja <pahuja.manan@outlook.com>
2026-08-29 19:00:57 +0530

    Update main.cpp
    final commit

--- cli-calculator/main.cpp
+++ cli-calculator/main.cpp
@@ -4,49 +4,50 @@
 using namespace std;
 
 int main() {
+    cout <<  "\t\tWELCOME TO CLI-CALCULATOR\n";
     double num1;
     double num2;
     string operation;
     bool calculation = true;
     while (calculation) {
 
-        cout << "\tEnter the operation(+,-,*,/,%,pow,sqrt,exit) : ";
+        cout << "\nEnter the operation(+,-,*,/,%,pow,sqrt,exit) : ";
         cin >> operation;
         if (operation == "exit") {
             calculation = false;
             break;
         }
-        cout << "Enter the number: ";
+        cout << "\nEnter the number: ";
         cin >> num1;
         if (operation != "sqrt") {
-      
-            cout << "\nEnter another number: ";
+
+            cout << "Enter another number: ";
             cin >> num2;
         }
         if (operation == "+") {
-            cout << num1 + num2 << endl;
-        
+            cout << "The output is : " << num1 + num2 << endl;
+
         }
         else if (operation == "-") {
-            cout << num1 - num2 << endl;
+            cout << "The output is : " << num1 - num2 << endl;
         }
         else if (operation == "*") {
-            cout << num1 * num2 << endl;
+            cout << "The output is : " << num1 * num2 << endl;
         }
         else if (operation == "/") {
-            cout << num1 / num2 << endl;
+            cout << "The output is : " << num1 / num2 << endl;
         }
         else if (operation == "%") {
             double remainder = fmod(num1, num2); 
-            cout << remainder << endl;
+            cout << "The output is : " << remainder << endl;
         }
         else if (operation == "pow") {
             double pow1 = pow(num1, num2); 
-            cout << pow1 << endl;
+            cout << "The output is : " << pow1 << endl;
         }
         else if (operation == "sqrt") {
             double sqrt1 = sqrt(num1); 
-            cout << sqrt1 << endl;
+            cout << "The output is : " << sqrt1 << endl;
         }
     }
     return 0;
