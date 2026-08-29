849177b8aca4f4d7ebbed4351fd6dc1187b5cb27
manan-pahuja <pahuja.manan@outlook.com>
2026-08-29 12:41:11 +0530

    Update main.cpp

--- cli-calculator/main.cpp
+++ cli-calculator/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <cmath>
+for namespace as std
 
 int main() {
-    
+    int num1;
+    int num2;
+    char operation;
+    bool calculation = true;
+    while (calculation);
+        cout << "Enter the number: ";
+        cin >> num1;
+        cout << "\nEnter another number: "
+        cin >> num2;
+        cout << "\tEnter the operation(+,-,*,/,%,pow,sqrt) : "
+        cin >> operation;
+        
+
     return 0;
 }
