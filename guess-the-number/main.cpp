c06baaadc5596ad5832432b5553b1da0beadda37
manan-pahuja <pahuja.manan@outlook.com>
2026-08-30 16:13:38 +0530

    Update main.cpp

--- guess-the-number/main.cpp
+++ guess-the-number/main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "\t\tWELCOME TO NUMBER GUESSING GAME\n";
+    cout << "\t\tWELCOME TO NUMBER GUESSING GAME\n" << "\nInstructions:-\nComputer will guess a number from 1 to 100 \nYou have to guess the number in 10 chances\nyou will be getting hint after the guess\n\n";
+    
+    bool playing = true;
+    while (playing) {
+        for(int guess = 10; guess > 0; --guess) {
+        cout << "guesses left:" << guess;
+        }
+    }
     return 0;
 }
