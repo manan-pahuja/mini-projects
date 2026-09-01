721444c4404fc245ecc868584b542bebe2729611
manan-pahuja <pahuja.manan@outlook.com>
2026-09-01 10:51:49 +0530

    Update main.cpp
    code logic

--- guess-the-number/main.cpp
+++ guess-the-number/main.cpp
@@ -4,12 +4,37 @@ using namespace std;
 
 int main() {
     cout << "\t\tWELCOME TO NUMBER GUESSING GAME\n" << "\nInstructions:-\nComputer will guess a number from 1 to 100 \nYou have to guess the number in 10 chances\nyou will be getting hint after the guess\n\n";
-    
+    int guess;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distrib(1, 100);
     bool playing = true;
     while (playing) {
-        for(int guess = 10; guess > 0; --guess) {
-        cout << "guesses left:" << guess;
+        int secret = distrib(gen);
+        for(int guesses = 10; guesses > 0; --guesses) {
+            cout << "guesses left:" << guesses;
+            if (guess == secret) {
+                cout << "\n\n\t\tYOU WON!! ";
+            }
+            else if (guess > secret) {
+                cout << "too high\n";
+            }
+            else if (guess < secret) {
+                cout << "too low\n";
+            }
         }
+        char choice;
+        cout << "\nWould you like to play another turn yes(y) no(n); "
+        cin >> choice;
+        if (choice == "y") {
+            continue;
+        }
+        else if (choice == " n") {
+            cout << "Thank you! for playing";
+            return 0;
+        
+        }
+        
     }
     return 0;
 }
