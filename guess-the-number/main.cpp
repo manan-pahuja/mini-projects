a5ef9e63c33ba7e711f3ecbed708e1edec7155e0
manan-pahuja <pahuja.manan@outlook.com>
2026-09-02 21:32:02 +0530

    Update main.cpp
    fixing a issue

--- guess-the-number/main.cpp
+++ guess-the-number/main.cpp
@@ -3,43 +3,54 @@
 using namespace std;
 
 int main() {
-    cout << "\t\tWELCOME TO NUMBER GUESSING GAME\n" << "\nInstructions:-\nComputer will guess a number from 1 to 100 \nYou have to guess the number in 10 chances\nyou will be getting hint after the guess\n\n";
-    int guess;
+    cout << "\t\tWELCOME TO NUMBER GUESSING GAME\n" 
+         << "\nInstructions:-\n"
+         << "I will choose a number from 1 to 100.\n" 
+         << "You have to guess the number in 10 chances.\n"
+         << "You will get hints after each guess!\n\n";
+
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> distrib(1, 100);
     bool playing = true;
-    
+
     while (playing) {
         int secret = distrib(gen);
-        for(int guesses = 10; guesses > 0; --guesses) {
-            cout << "guesses left:" << guesses;
-            
-            // NOTE: You still need a `cin >> guess;` inside this loop 
-            // so the user can actually enter their guess!
-            
+        int guess = 0;
+        bool won = false;
+
+        for (int guesses = 10; guesses > 0; --guesses) {
+            cout << "\nGuesses left: " << guesses << ". Enter your guess: ";
+            cin >> guess; // Added the missing input statement
+
             if (guess == secret) {
-                cout << "\n\n\t\tYOU WON!! ";
-                break; // Optional: break out of the guessing loop if they win
+                cout << "\n\t\tYOU WON!! You guessed the right number!\n";
+                won = true;
+                break; 
             }
             else if (guess > secret) {
-                cout << "too high\n";
+                cout << "Too high!\n";
             }
-            else if (guess < secret) {
-                cout << "too low\n";
+            else {
+                cout << "Too low!\n";
             }
         }
-        
+
+        // Handle case where player runs out of guesses
+        if (!won) {
+            cout << "\n\t\tGAME OVER! You ran out of guesses. The secret number was " << secret << ".\n";
+        }
+
         char choice;
-        cout << "\nWould you like to play another turn yes(y) no(n): "; // Fixed missing semicolon here
+        cout << "\nWould you like to play another turn? yes(y) / no(n): ";
         cin >> choice;
-        
-        if (choice == 'y') { // Fixed: Used single quotes '' for char
+
+        if (choice == 'y' || choice == 'Y') {
             continue;
         }
-        else if (choice == 'n') { // Fixed: Used single quotes and removed the extra space
-            cout << "Thank you! for playing";
-            return 0;
+        else {
+            cout << "Thank you for playing!\n";
+            break;
         }
     }
     return 0;
